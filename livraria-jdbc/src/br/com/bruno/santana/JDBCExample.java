c39a92b0ee09a62d09e6174e68e8e42360e80fd5
elizauema <eliza.uema@gmail.com>
2021-05-05 20:25:45 -0300

    incl. do delete e select

--- livraria-jdbc/src/br/com/bruno/santana/JDBCExample.java
+++ livraria-jdbc/src/br/com/bruno/santana/JDBCExample.java
@@ -2,6 +2,7 @@ package br.com.bruno.santana;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,11 +14,18 @@ public class JDBCExample {
 
 	// Database credentials
 	static final String USER = "root";
-	static final String PASS = "123456";
+	static final String PASS = "01061966";
 
 	public static void main(String[] args) {
+		//insert();
+		//update();
+		//delete();
+		select();
+	}// end main
+	private static void insert() {
 		Connection conn = null;
 		Statement stmt = null;
+		
 		try {
 			// STEP 2: Register JDBC driver
 			Class.forName(JDBC_DRIVER);
@@ -31,7 +39,7 @@ public class JDBCExample {
 			System.out.println("Inserting records into the table...");
 			stmt = conn.createStatement();
 
-			String sql = "INSERT INTO livros VALUES ('Cold-Case Christianity', 'J. Warner Wallace')";
+			String sql = "INSERT INTO livros(titulo, autor) VALUES ('Cold-Case Christianity', 'J. Warner Wallace')";
 			stmt.executeUpdate(sql);
 			System.out.println("Inserted records into the table...");
 
@@ -56,6 +64,131 @@ public class JDBCExample {
 			} // end finally try
 		} // end try
 		System.out.println("Goodbye!");
-	}// end main
+	}
+	
+	private static void update() {
+		System.out.println("update"); 
+		// fazer um update
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName(JDBC_DRIVER);
 
+			// STEP 3: Open a connection
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connected database successfully...");
+
+			// STEP 4: Execute a query
+			System.out.println("Alterando os dados...");
+			stmt = conn.createStatement();
+
+			String sql = "UPDATE livros SET titulo ='JjjjjjjjjjjjjjjjJ' where titulo ='Cold-Case Christianity'";
+			stmt.executeUpdate(sql);
+			System.out.println("Update records into the table...");
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException se) {
+			} // do nothing
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Alterado!");
+	}
+	private static void delete() {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName(JDBC_DRIVER);
+
+			// STEP 3: Open a connection
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			stmt = conn.createStatement();
+
+			String sql = "DELETE from livros where titulo ='Cold-Case Christianity';";
+			stmt.executeUpdate(sql);
+			System.out.println("Deleted records ...");
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException se) {
+			} // do nothing
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Goodbye!");
+	}
+	private static void select() {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+	
+			Class.forName(JDBC_DRIVER);
+
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			stmt = conn.createStatement();
+			String sql = "SELECT * from livros;";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println("Titulo:"+rs.getString("titulo"));
+				System.out.println("Autor.:"+rs.getString("autor"));
+				System.out.println();
+			}
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException se) {
+			} // do nothing
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Goodbye!");
+	}
 }
