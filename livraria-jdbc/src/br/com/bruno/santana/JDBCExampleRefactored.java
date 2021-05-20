88578250ff0cad1624e510a375ee0b7d340f5ee9
elizauema <eliza.uema@gmail.com>
2021-05-20 20:37:47 -0300

    correcao do update e delete

--- livraria-jdbc/src/br/com/bruno/santana/JDBCExampleRefactored.java
+++ livraria-jdbc/src/br/com/bruno/santana/JDBCExampleRefactored.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JDBCExampleRefactored {
 
@@ -13,7 +14,7 @@ public class JDBCExampleRefactored {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost/livraria";
 	static final String USER = "root";
-	static final String PASS = "123456";
+	static final String PASS = "01061966";
 
 	public static void main(String[] args) {
 		executarQueries();
@@ -28,16 +29,45 @@ public class JDBCExampleRefactored {
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			stmt = conn.createStatement();
 
-			select(stmt);
+		//	select(stmt);
 			
-			insert(conn);
-			select(stmt);
+		//	insert(conn);
+		//	select(stmt);
 			
-			update(stmt);
-			select(stmt);
+		//	update(stmt);
+		//	select(stmt);
 			
-			delete(stmt);
-			select(stmt);
+		//	delete(stmt);
+		//	select(stmt);
+			int opcao = 0;
+			while (opcao !=5) {
+			 Scanner scanner = new Scanner(System.in);
+			 System.out.println("Selecione uma opção");
+			 System.out.println("1-Inclusão");
+			 System.out.println("2-Alterção");
+			 System.out.println("3-Exclusão");
+			 System.out.println("4-Consulta");
+			 System.out.println("5-Exit");
+			 opcao = scanner.nextInt();
+			 switch (opcao) {
+			 case 1:
+				insert(conn);
+				break;
+			 case 2:
+				 update(conn);
+				 break;
+			 case 3:
+				 delete(conn);
+				 break;
+			 case 4:
+				 select(stmt);
+				 break;	 
+			 case 5:
+			 	 break;
+			 }
+			 scanner.close();
+			}
+			
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -63,23 +93,51 @@ public class JDBCExampleRefactored {
 		System.out.println("insert...\n");
 		String sql = "INSERT INTO livros(titulo, autor) VALUES (?,?);";
 		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, "Felicidade Verdadeira");
-		ps.setString(2, "Heber Campos Jr.");
+//		ps.setString(1, "Felicidade Verdadeira");
+//		ps.setString(2, "Heber Campos Jr.");
+		Scanner scanner = new Scanner(System.in);
+		String newTitulo = "";
+		String newAutor = "";
+		System.out.println("Digite Titulo:");
+		newTitulo = scanner.nextLine();
+		System.out.println("Digite Autor.:");
+		newAutor = scanner.nextLine();
+		scanner.close();
+		ps.setString(1, newTitulo);
+		ps.setString(2, newAutor);
 		ps.executeUpdate();
 	}
 	
-	public static void update(Statement stmt) throws SQLException {
+	public static void update(Connection conn) throws SQLException {
 		System.out.println("update...\n");
-		String sql = "UPDATE livros SET titulo ='Outro título' where titulo ='Felicidade Verdadeira'";
-		stmt.executeUpdate(sql);
+		Scanner scanner = new Scanner(System.in);
+		String oldTitulo = "";
+		String newAutor = "";
+		System.out.println("Digite Titulo a alterar:");
+		oldTitulo = scanner.nextLine();
+		System.out.println("Digite novo autor......:");
+		newAutor = scanner.nextLine();
+		String sql = "UPDATE livros SET autor = ? where titulo = ?;";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, newAutor);
+		ps.setString(2, oldTitulo);
+		ps.executeUpdate();
+		scanner.close();
 	}
 	
-	public static void delete(Statement stmt) throws SQLException {
+	public static void delete(Connection conn) throws SQLException {
 		System.out.println("delete...\n");
-		String sql = "DELETE from livros where titulo ='Outro título';";
-		stmt.executeUpdate(sql);
+		Scanner scanner = new Scanner(System.in);
+		String delTitulo = "";
+		System.out.println("Digite Titulo a excluir:");
+		delTitulo = scanner.nextLine();
+		String sql = "DELETE from livros where titulo = ?;";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, delTitulo);
+		ps.executeUpdate();
+		scanner.close();
 	}
-	
+		
 	public static void select(Statement stmt) throws SQLException {
 		String sql = "SELECT * from livros;\n";
 		ResultSet rs = stmt.executeQuery(sql);
