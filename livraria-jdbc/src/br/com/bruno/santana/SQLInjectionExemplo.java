8cd5083f1a915b4ab5c90723c5d0f309510df705
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-05-25 20:26:44 -0300

    Exemplo de SQL Injection.

---
+++ livraria-jdbc/src/br/com/bruno/santana/SQLInjectionExemplo.java
@@ -0,0 +1,86 @@
+package br.com.bruno.santana;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Mais exemplos em: https://riptutorial.com/jdbc/example/28671/statement---sql-injection-evil
+public class SQLInjectionExemplo {
+
+	//Dados do Banco
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/livraria?allowMultiQueries=true";
+	static final String USER = "root";
+	static final String PASS = "root1234";
+
+	public static void main(String[] args) {
+		executarQueries();
+	}
+	
+	private static void executarQueries() {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = conn.createStatement();
+
+			insert(stmt, "Finalmente Vivos", "John Piper");
+			insert(stmt, "Coronavírus e Cristo", "John Piper");
+			insert(stmt, "Java 9", "Rodrigo Turini");
+			select(stmt);
+			
+			selectComSqlInjection(stmt, "Rodrigo Turini");
+			select(stmt);
+			
+			selectComSqlInjection(stmt, "Rodrigo Turini';delete from livros; #");
+			select(stmt);
+			
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					conn.close();
+			} catch (SQLException e) {
+			} // do nothing
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Fim!");
+	}
+	
+	public static void insert(Statement stmt, String titulo, String autor) throws SQLException {
+		System.out.println("insert...\n");
+		String sql = "INSERT INTO livros(titulo, autor) VALUES ('" + titulo + "', '" + autor + "')";
+		stmt.executeUpdate(sql);
+	}
+	
+	public static void select(Statement stmt) throws SQLException {
+		String sql = "SELECT * from livros;\n";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			System.out.println("Titulo: "+rs.getString("titulo"));
+			System.out.println("Autor: "+rs.getString("autor"));
+			System.out.println();
+		}
+		System.out.println("#################################");
+	}
+	
+	public static void selectComSqlInjection(Statement stmt, String autor) throws SQLException {
+		System.out.println("select com vulnerabilidade...\n");
+		String sql = "SELECT * from livros where autor = '" + autor + "'";
+		stmt.executeQuery(sql);
+	}
+	
+}
