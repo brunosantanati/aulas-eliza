eba6f1e55f8f4a29b267086fdb3fc034bfd012d7
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-08-05 19:57:22 -0300

    Exemplos usando Thymeleaf (th:text e th:switch)

---
+++ spring-thymeleaf/src/main/java/me/brunosantana/controller/MyController.java
@@ -0,0 +1,17 @@
+package me.brunosantana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Mundo!");
+        model.addAttribute("status", "ENTREGUE");
+        return "hello";
+    }
+
+}
---
+++ spring-thymeleaf/src/test/java/me/brunosantana/SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brunosantana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
